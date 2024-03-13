45a83c58fa385b5fdda190f97873c03610ca73f1
CristianEstrada <ecristian963@gmail.com>
2024-03-13 10:31:44 -0500

    loginform add

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+</script>
+<template>
+    <form name="login-form" >
+          <div class="mb-3">
+            <label for="email">email: </label>
+            <input id="email" type="text" />
+          </div>
+          <div class="mb-3">
+            <label for="password">Password: </label>
+            <input id="password" type="password" />
+          </div>
+          <button class="btn btn-outline-dark" type="submit">
+            Login
+          </button>
+        </form>
+</template>
